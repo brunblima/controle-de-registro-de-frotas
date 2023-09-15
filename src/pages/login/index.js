c03303ef0e49a474c1d5716800ec7c93fc1ae70c
Bruno Lima <blima@ane.local>
2023-09-15 17:00:02 -0300

    README UPDATE

---
+++ src/pages/login/index.js
@@ -0,0 +1,18 @@
+import React from "react";
+import {View, Text, TextInput, StyleSheet} from 'react-native'
+
+export default function Login(){
+    return(
+      <View style={styles.container}>
+  
+      </View>
+    )
+  }
+  const styles = StyleSheet.create({
+    container: {
+      flex: 1,
+    
+    },
+  });
+  
+  
